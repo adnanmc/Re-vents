ffa19b320a51eb05cf4a9bf012e1ce62ad9ab86a
adnanmc <adnanmcj@gmail.com>
2018-11-27 12:11:36 -0500

    added baseline layout

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import { Grid } from 'semantic-ui-react';
+import EventList from '../EventList/EventList'
 
 class EventDashboard extends Component {
   render() {
     return (
       <Grid>
-          <Grid.column width={10}>
-            <h2>Left column</h2>
-          </Grid.column>
-          <Grid.column width={6}>
-            <h2>Right column</h2>
-          </Grid.column>
+        <Grid.Column width={10}>
+          <EventList/>
+        </Grid.Column>
+        <Grid.Column width={6}>
+          <h2>Right column</h2>
+        </Grid.Column>
       </Grid>
-    )
+    );
   }
 }
 
